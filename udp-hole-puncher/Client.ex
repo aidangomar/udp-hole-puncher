8f44abe4149f20f4821dfe7038675c08f2ac287b
Aidan <64613310+susuwatarii@users.noreply.github.com>
2022-09-05 18:39:36 -0400

    Update Client.ex

--- udp-hole-puncher/Client.ex
+++ udp-hole-puncher/Client.ex
@@ -11,15 +11,10 @@ defmodule Client do
   end
 
 
-  def keepalive_daemon(c0_port, c1_port, c1_ip) do
-    {:ok, socket} = :gen_udp.open(c0_port)
+  def keepalive_daemon(socket, c1_port, c1_ip) do
     :gen_udp.send(socket, c1_ip, c1_port, "KEEP_ALIVE")
     :timer.sleep(30 * 1000)
-    keepalive_daemon(c0_port, c1_port, c1_ip)
-  end
-
-  def convert_ip_to_tuple(ip) do
-    String.split(ip, ".") |> Enum.map(fn x -> Integer.parse(x) |> case do {y, _} -> y end end) |> List.to_tuple
+    keepalive_daemon(socket, c1_port, c1_ip)
   end
 
   def gen_port() do
@@ -28,12 +23,25 @@ defmodule Client do
   end
 
   def terminal_interface() do
+
     IP = IO.gets "Enter the IP you would like to talk to: "
+      |> String.trim("\n")
+      |> String.split(".")
+      |> Enum.map(fn x -> Integer.parse(x) |>
+          case do
+            {y, _} -> y
+          end
+        end)
+      |> List.to_tuple
+
     c0_port = gen_port()
     IO.puts("Your port is #{c0_port}")
-    c1_port = IO.gets("When ready, enter your friend's port: ")
-    keepalive_daemon(c0_port, c1_port, IP)
+    c1_port = IO.gets("When ready, enter your friend's port: ") |> String.trim("\n")
+
+    {:ok, socket} = :gen_udp.open(c0_port)
+    keepalive_daemon(socket, c1_port, IP)
     IO.puts("success!")
+
   end
 
 end
